f32a56906d7a9abc3f2f2e5e692351913e9b38bf
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-08 02:46:53 -0500

    Cambia de color segun el presupuesto restante

--- src/componentes/Restante.js
+++ src/componentes/Restante.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import {revisarPresupuesto} from '../helper'
 
 class Restante extends Component{
     render() {
+        const {presupuesto,restante}=this.props;
         return (
-            <div className="alert alert-success">
+            <div className= {revisarPresupuesto(presupuesto,restante)}>
                Restante $ {this.props.restante} 
             </div>
         );
